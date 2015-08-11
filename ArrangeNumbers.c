0f35730aa5a67d7e177d0380233d0fc26ecb8507
totalintelli <kwanwoo5@naver.com>
2015-08-11 12:13:11 +0900

    ArrangeNumbers.c 파일 생성

---
+++ ArrangeNumbers.c
@@ -0,0 +1,58 @@
+// ArrangeNumbers.c
+/*
+파일 이름: ArrangeNumbers.c
+기     능: 입력받은 수를 크기 순으로 정렬한다.
+작  성 자: 송 용 단
+작성 일자: 2015년 8월 10일
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX 3
+typedef signed long int Long;
+
+int main(int argc, char *argv[]);
+void Input(Long(*numbers));
+void ArrangeNumbers(Long(*numbers));
+void Output(Long(*numbers));
+
+int main(int argc, char *argv[]) {
+	Long numbers[MAX];
+
+	Input(numbers);
+	ArrangeNumbers(numbers);
+	Output(numbers);
+
+	return 0;
+}
+
+void Input(Long(*numbers)) {
+	Long i;
+
+	for (i = 0; i < MAX; i++) {
+		scanf("%d", numbers + i);
+	}
+}
+
+void ArrangeNumbers(Long(*numbers)) {
+	Long temp;
+	Long i;
+	Long j;
+
+	for (i = 0; i < MAX - 1; i++) {
+		for (j = i + 1; j < MAX; j++) {
+			if (numbers[i] < numbers[j]) {
+				temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
+			}
+		}
+	}
+}
+
+void Output(Long(*numbers)) {
+	Long i;
+
+	for (i = 0; i < MAX; i++) {
+		printf("%d\n", numbers[i]);
+	}
+}
